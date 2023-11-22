dfa124f4058e5e0226221e4ec07d5ee7dcdca4a0
akhademik <hajtran@gmail.com>
2023-11-22 12:28:45 +0700

    feat: add i18n and some alias

--- front-end/src/routes/+layout.svelte
+++ front-end/src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import CSeo from '$common/c-seo.svelte'
+	import { BaseSeo } from '$base'
 	import { seo } from '$lib/utils/seo-info'
 
 	import '$lib/styles/main.css'
 	import '$lib/styles/tailwind.css'
 </script>
 
-<CSeo
+<BaseSeo
 	title="CHD Travel - 2023"
 	description={seo.description}
 	keywords={seo.keywords} />
