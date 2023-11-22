7586610f950877f1623ae638053a8bc68bc758eb
akhademik <hajtran@gmail.com>
2023-11-22 10:14:24 +0700

    refactor: make seo metadata
    Make a separate metadata for later usage of SEO.
    Add the favicon for CHD

--- front-end/src/routes/+layout.svelte
+++ front-end/src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import CSeo from '$common/c-seo.svelte'
+	import { seo } from '$lib/utils/seo-info'
 
 	import '$lib/styles/main.css'
 	import '$lib/styles/tailwind.css'
 </script>
 
 <CSeo
-	title="Svelte Boiler Plate"
-	description="Svelte Boiler Plate" />
+	title="CHD Travel - 2023"
+	description={seo.description}
+	keywords={seo.keywords} />
 
 <slot />
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#003851',
+				secondary: '#e77916',
+			},
+			fontFamily: {
+				roboto: ['Roboto', 'sans-serif'],
+				figtree: ['Figtree', 'sans-serif'],
+			},
+		},
 		fontFamily: {
-			roboto: ["Roboto", "sans-serif"]
-		}
+			roboto: ['Roboto', 'sans-serif'],
+		},
 	},
-	plugins: []
-};
+	plugins: [],
+}
