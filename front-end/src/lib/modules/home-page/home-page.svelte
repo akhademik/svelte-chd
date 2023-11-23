c1b5796aafad59cb1cd1dcd941e333df5e75086f
akhademik <hajtran@gmail.com>
2023-11-23 16:12:21 +0700

    feat: done homepage and typewriter effect
    done homepage and finished the typewriter effect
    in homepage for the slogan

--- front-end/src/lib/modules/home-page/home-page.svelte
+++ front-end/src/lib/modules/home-page/home-page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import hero from '$assets/imgs/hero-elephant.jpg'
+	import { BaseTypewriter } from '$base'
 	import LL from '$i18n/i18n-svelte'
 
 	import HomeHeadline from './components/home-headline.svelte'
@@ -9,12 +10,7 @@
 	<section class="flex flex-col justify-center gap-5 px-2 md:gap-10">
 		<HomeHeadline />
 		<p class="text-lg md:text-2xl">{$LL.home_page.intro()}</p>
-		<div class="text-center [&>div]:font-bold [&>div]:uppercase">
-			<!-- <VueWriter
-                        :array="[LL.home_page.slogan()]"
-                        :type-speed="60"
-                        :erase-speed="60" /> -->
-		</div>
+		<BaseTypewriter text={$LL.home_page.slogan()} />
 	</section>
 	<section class="flex items-center justify-center">
 		<div
